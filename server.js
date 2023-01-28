8f29c202936d96e3faf3cd42ec5c3d4ff8503063
Glitch (glitch-hello-node) <none>
2023-01-28 17:03:52 +0000

    📍🌵 Checkpoint ./server.js:53640836/2309

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const fastify = require("fastify")({
 
 const mongoose = require('mongoose');
 const dotenv=require('dotenv');
+
 mongoose.set('strictQuery', false);
 const autopopulate = require('mongoose-autopopulate');
 dotenv.config();
@@ -18,6 +19,7 @@ mongoose.Promise = global.Promise;
 mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
 const Schema = mongoose.Schema;
 const Types = mongoose.Types;
+const model=mongoose.model;
 
 let Orders_schema = new Schema({
     orderDate: Date,
@@ -27,11 +29,11 @@ let Orders_schema = new Schema({
         autopopulate: true
     },
     item: [{
-        // product: {
-        //     type: Schema.Types.ObjectId,
-        //     ref: 'Prices',
-        //     autopopulate: true
-        // },
+        product: {
+            type: Schema.Types.ObjectId,
+            ref: 'Prices',
+            autopopulate: true
+        },
         volume: Number,                 // Литры
     }],
     // pail: {                         // ведро
@@ -43,9 +45,87 @@ let Orders_schema = new Schema({
     appendix: String
 });
 Orders_schema.plugin(autopopulate);
-const Order = mongoose.model('Order', Orders_schema);
 
+Orders_schema.statics.findByClientAndDate = async function (id, date) {
+    return this.find({ client: Types.ObjectId(id), orderDate: date });
+}
+Orders_schema.statics.findOneByClientAndDate = async function (id, date) {
+    return await this.findOne({ 'client': id, 'orderDate': date });
+}
+Orders_schema.statics.findBySalerAndDate = async function (id, date) {
+    let goods = model('Prices').find({ 'saler': id, 'active': true });
+    return await this.find({ 'item': { $in: goods }, 'orderDate': date });
+}
 
+let client_schema = new Schema({
+    login: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    password: String,
+    fullName: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    phone: String,
+    town: String,
+    street: String,
+    house: String,
+    entrance: String,
+    appartment: String,
+    addition: String,                // код домофона 
+    role: {
+        type: String,
+        enum: ['buyer', 'saler'],
+        default: 'buyer',
+        requred: true
+    }
+});
+
+client_schema.virtual('fullAddress')
+    .get(function () {
+        return `${this.town}, ${this.street}, д. ${this.house}`
+            + (!this.entrance ? "" : `, подъезд ${this.entrance} `)
+            + (!this.appartment ? "" : `, кв. ${this.appartment}`)
+            + (!this.addition ? "" : `, ${this.addition}`).trim();
+    });
+
+let price_schema = new Schema({
+    product: {
+        type: String,
+        trim: true
+    },
+    price: Number,
+    active: {
+        type: Boolean,
+        default: true
+    },
+    client: { // продавец
+        type: Schema.Types.ObjectId,
+        ref: 'Clients',
+        autopopulate: true
+    }
+});
+
+price_schema.statics.pricesBySaler = async function (saler) {
+    return this.find({ client: Types.ObjectId(saler) });
+}
+
+price_schema.statics.toAssociativeArray = async function () {
+    let ret = {};
+    let p = await this.find({ active: true });
+    p.forEach(item => {
+        ret[item._id] = item;
+        // ret[item.product] = item;
+    });
+    return ret;
+};
+
+const Order = model('Order', Orders_schema);
+const Price = model('Price', price_schema);
+const Client = model('Client' client_schema);
 
 // ADD FAVORITES ARRAY VARIABLE FROM TODO HERE
 
